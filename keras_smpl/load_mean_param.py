0952d737606aed9cc30945e37975c5bbf4bc165d
Akash Sengupta <akash.sengupta1997@gmail.com>
2019-02-15 13:32:22 +0000

    Update load_mean_param.py

--- keras_smpl/load_mean_param.py
+++ keras_smpl/load_mean_param.py
@@ -38,6 +38,7 @@ def concat_mean_param(img_features):
     # mean_pose[0] = np.pi
 
     mean = np.expand_dims(np.hstack((mean_pose, mean_shape)), axis=0)
+    mean = np.zeros((1, 82))
     mean = tf.constant(mean, dtype='float32')
     # print(mean.shape)
     mean = tf.tile(mean, [K.shape(img_features)[0], 1])
