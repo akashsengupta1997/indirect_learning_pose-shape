5895bab9139471bf9f19bd227e94280be090b4ad
Akash Sengupta <akash.sengupta1997@gmail.com>
2019-05-13 13:12:24 +0100

    Update evaluate_autoencoder.py

--- evaluate_autoencoder.py
+++ evaluate_autoencoder.py
@@ -141,8 +141,74 @@ def evaluate_autoencoder_iou_and_acc(eval_dir, input_wh, output_wh, num_classes,
     accuracy = total_correct_predicts / (output_wh * output_wh * num_eval_images)
     print("Overall accuracy over dataset", accuracy)
 
-evaluate_autoencoder_iou_and_acc("/Users/Akash_Sengupta/Documents/4th_year_project_datasets/up-s31/s31_padded_small_glob_rot/val_masks/val",
-                                 256,
-                                 48,
-                                 32,
-                                 'up-s31_48x48_resnet_ief_scaledown0005_arms_weighted2_bg_weighted_0point3_gamma2_690.hdf5')
+
+def evaluate_autoencoder_from_enet_segs_iou_and_acc(enet_segs_dir, gt_segs_dir, input_wh, output_wh,
+                                                    num_classes, model_fname):
+    smpl_model = load_model(os.path.join("./autoencoder_weights", model_fname),
+                            custom_objects={'dd': dd,
+                                            'tf': tf})
+    print('Model {model_fname} loaded'.format(model_fname=model_fname))
+
+    verts_model, projects_model, segs_model = build_full_model(smpl_model,
+                                                               output_wh,
+                                                               "./neutral_smpl_with_cocoplus_reg.pkl")
+    total_intersects_per_class = np.zeros(num_classes - 1)
+    total_unions_per_class = np.zeros(num_classes - 1)
+    total_correct_predicts = 0
+    num_eval_images = len([fname for fname in sorted(os.listdir(enet_segs_dir)) if
+                           fname.endswith(".png")])
+    for fname in sorted(os.listdir(enet_segs_dir)):
+        if fname.endswith(".png"):
+            print(fname)
+            input_seg_map = load_input_seg(enet_segs_dir, fname, input_wh, num_classes)  # (1, input_wh, input_wh, 1)
+            segs = segs_model.predict(input_seg_map)  # (1, output_wh, output_wh, 32)
+            predicted_seg_map = np.argmax(segs, axis=-1)
+            predicted_seg_map = predicted_seg_map[0]  # (output_wh, output_wh)
+            ground_truth_seg_map = cv2.resize(cv2.imread(os.path.join(gt_segs_dir,
+                                                                      fname[:5] + "_ann.png"),
+                                                         0),
+                                              (output_wh, output_wh),
+                                              interpolation=cv2.INTER_NEAREST)  # (output_wh, output_wh)
+
+            num_intersects_per_class, num_unions_per_class = compute_intersection_and_union(
+                ground_truth_seg_map,
+                predicted_seg_map,
+                num_classes)
+            print("I/Us for image", num_intersects_per_class, num_unions_per_class)
+            total_intersects_per_class += num_intersects_per_class
+            total_unions_per_class += num_unions_per_class
+            print("Total I/Us", total_intersects_per_class, total_unions_per_class)
+
+            num_correct_predicts = count_correct_predicts(ground_truth_seg_map,
+                                                          predicted_seg_map)
+            total_correct_predicts += num_correct_predicts
+            print("Correct predicts for image", num_correct_predicts)
+            print("Total correct predicts", total_correct_predicts)
+
+            # plt.figure(1)
+            # plt.subplot(311)
+            # plt.imshow(input_seg_map[0, :, :, 0])
+            # plt.subplot(312)
+            # plt.imshow(predicted_seg_map)
+            # plt.subplot(313)
+            # plt.imshow(ground_truth_seg_map)
+            # plt.show()
+
+    ious = np.divide(total_intersects_per_class, total_unions_per_class)
+    mean_iou = np.mean(ious)
+    print("Mean IOU", mean_iou)
+    accuracy = total_correct_predicts / (output_wh * output_wh * num_eval_images)
+    print("Overall accuracy over dataset", accuracy)
+
+# evaluate_autoencoder_iou_and_acc("/Users/Akash_Sengupta/Documents/4th_year_project_datasets/up-s31/s31_padded_small_glob_rot/val_masks/val",
+#                                  256,
+#                                  48,
+#                                  32,
+#                                  'up-s31_48x48_resnet_ief_scaledown0005_arms_weighted2_bg_weighted_0point3_gamma2_690.hdf5')
+
+evaluate_autoencoder_from_enet_segs_iou_and_acc("/Users/Akash_Sengupta/Documents/4th_year_project_datasets/up-s31/s31_padded_small_glob_rot_enet_segs",
+                                                "/Users/Akash_Sengupta/Documents/4th_year_project_datasets/up-s31/s31_padded_small_glob_rot/val_masks/val",
+                                                256,
+                                                48,
+                                                32,
+                                                'up-s31_48x48_resnet_ief_scaledown0005_arms_weighted2_bg_weighted_0point3_gamma2_690.hdf5')
